5b4bf5c4429d6fe670aed31a0a9b4d9a7a792bf0
andrew <94652895+Mulberry03@users.noreply.github.com>
2022-12-20 22:28:12 -0300

    Musica enemigos, entorno luz

---
+++ Assets/Scripts/Cescena.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Cescena : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene("1");
+    }
+}
